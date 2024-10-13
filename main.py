4cf49e37f0ad01efc5f1f0785f396a5553286d51
elik <elik@example.com>
2024-10-13 11:17:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel, Field, field_validator, EmailStr
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse("/docs")
+
+
+class User(BaseModel):
+    name: str = Field(min_length=2)
+    password: str = Field(min_length=2)
+    surname: str = Field(min_length=2)
+    email: EmailStr
+
+    @field_validator("name", "surname")
+    @classmethod
+    def name_surname(cls, v: str):
+        if not v.isalpha():
+            raise ValueError("name or surname can only have letters")
+        if len(v) < 2:
+            raise ValueError("len of name or surname not have  2 letters")
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, v):
+        if len(v) < 8:
+            raise ValueError("password must have minimum 8 letters")
+        if not any(ch.isupper() for ch in v):
+            raise ValueError("password must have one capital letter")
+        if not any(ch.islower() for ch in v):
+            raise ValueError("password must have one small letter")
+        if all(ch.isalnum() for ch in v):
+            raise ValueError("password must have one special character")
+        if not any(ch.isdigit() for ch in v):
+            raise ValueError("password must have one number")
+
+
+@app.post("/register")
+async def register_user(user: User):
+    return {"message": "User is created!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
